4ff6f184270f483e63df4bfa95bda44ed8343790
Bereket Semagn <semagnbereket@gmail.com>
2023-10-07 17:02:00 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mt1.google.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
