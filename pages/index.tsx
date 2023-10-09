39bfaeae54d948ecdd1c03d0d822b88293abec35
Bereket Semagn <semagnbereket@gmail.com>
2023-10-08 21:11:06 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,20 +9,20 @@ const Leaflet = dynamic(() => import("../components/Leaflet"), {
 
 const Home = ({ data }: { data: LeafletData[] }) => {
   console.log(data);
-  
+
   return <Leaflet data={data} />;
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await fetch(`http://127.0.0.1:5000/data?queryData=cfc13`).then(
-    (res) => res.json()
-  );
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const data = await fetch(`http://127.0.0.1:5000/data?queryData=cfc13`).then(
+//     (res) => res.json()
+//   );
 
-  return {
-    props: {
-      data,
-    },
-  };
-};
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
 
 export default Home;
