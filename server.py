fffb70fa59db66726c4bc9dadde60d19d148fbcc
zanzilla22 <69683242+zanzilla22@users.noreply.github.com>
2023-10-08 22:38:10 -0400

    ozoineeee
    we fixed the ozone layer with this one

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template, request, send_from_directory
 import requests, csv, json
 from bs4 import BeautifulSoup
 from io import StringIO
+import re
 
 app = Flask(__name__)
 
@@ -21,7 +22,31 @@ CFC_areaCoords = {
     "psa": (-64.6, -64.0),
     "spo": (-90, 0),  # South Pole (90S) can be represented as (90, 0)
 }
-@app.route('/get_flux_data', methods=['GET'])
+def get_ozone_data():
+    url = "https://ozonewatch.gsfc.nasa.gov/data/omi/Y2023/L3_ozone_omi_20231006.txt" #could constantly update that last number but also no
+    response = requests.get(url)
+    # print(response)
+    # return("eh")
+    lines = response.text[244:].replace("\n", "").split(" lat = ")
+
+    result = [[int(x.replace(' ', '0')) for x in lines[0][7:].strip(' ')]]
+    for i in range(1, len(lines)):
+        result.append(float(lines[i][1:6]))
+        result.append([
+            int(x.replace(' ', '0')) for x in lines[i][7:].strip(' ')
+        ])
+    # Combine latitudes and number sequences into a list of tuples
+
+    yCoeff = 3
+    mapRes = []
+
+    for i in range(0, len(result)-1, 2):
+        lat = result[i+1]
+        longs = result[i]
+        for long in range(len(longs)):
+            mapRes.append([lat, int((long-180*3)/yCoeff), longs[long]])
+
+    return(mapRes)
 def get_flux_data():
     print("trying to get flux data")
     url = 'https://spaceweather.gc.ca/forecast-prevision/solar-solaire/solarflux/sx-5-flux-en.php'
@@ -40,12 +65,12 @@ def get_flux_data():
             last_row_data = [cell.text.strip() for cell in last_row.find_all('td')]
 
             print(last_row_data)
-            return jsonify(last_row_data)
+            return (last_row_data)
 
     else:
         print("site reference was goodnt")
 
-    return jsonify({'error': 'Table not found'}), 404
+    return 'Table not found'
 
 
 def sendData(queryTerms):
@@ -69,7 +94,14 @@ def sendData(queryTerms):
 
         queryData = pullData(query)
         for d in queryData:
-            data["points"].append([x for x in d]+[queryData[d]])
+            if(query[:3] == "cfc"):
+                data["points"].append([x for x in d]+[queryData[d]])
+
+            elif(query == "solarFlux"):
+                data["points"] = [float(queryData[-2])]
+
+            else:
+                data["points"] = queryData
 
         bigData.append(data)
 
@@ -91,43 +123,49 @@ def pullData(query):
         'cfc13': "https://gml.noaa.gov/aftp/data/hats/cfcs/cfc113/combined/HATS_global_F113.txt",
     }
 
-    response = requests.get(CFC_link_index[query])
+    if(query[:3] == "cfc"):
+        response = requests.get(CFC_link_index[query])
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Convert the response content (CSV data) into a string
-        csv_data = response.text
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Convert the response content (CSV data) into a string
+            csv_data = response.text
 
-        # Use StringIO to treat the string as a file-like object
-        csv_file = StringIO(csv_data)
+            # Use StringIO to treat the string as a file-like object
+            csv_file = StringIO(csv_data)
 
-        # Create a CSV reader object
-        csv_reader = csv.reader(csv_file)
+            # Create a CSV reader object
+            csv_reader = csv.reader(csv_file)
 
-        rows = list(csv_reader)
+            rows = list(csv_reader)
 
-        # header row is index 67
-        print("Header row i think idk")
-        # both length 33
-        header = rows[67][0].split()
-        latestFilledData = 0
-        for i in range(len(rows) - 1, -1, -1):
-            if 'nan' not in rows[i][0].split():
-                latestFilledData = (rows[i][0].split())
-                break  # Stop looking further
+            # header row is index 67
+            print("Header row i think idk")
+            # both length 33
+            header = rows[67][0].split()
+            latestFilledData = 0
+            for i in range(len(rows) - 1, -1, -1):
+                if 'nan' not in rows[i][0].split():
+                    latestFilledData = (rows[i][0].split())
+                    break  # Stop looking further
 
-        # print(header[8].split('_')[1])
-        # data = {(CFC_areaCoords[header[i].split('_')[1]] if header[i].split('_')[1] in [key for key in CFC_areaCoords] and len(header[i].split('_')) == 3 else header[i]): latestFilledData[i] for i in range(len(header))}
-        data = {(CFC_areaCoords[header[i].split('_')[1]] if header[i].split('_')[1] in [key for key in
-                                                                                        CFC_areaCoords] and len(
-            header[i].split('_')) == 3 else ""): float(latestFilledData[i]) for i in range(len(header))}
-        del data[""]
-        print(data)
-        return(data)
+            # print(header[8].split('_')[1])
+            radMult = 15
+            # data = {(CFC_areaCoords[header[i].split('_')[1]] if header[i].split('_')[1] in [key for key in CFC_areaCoords] and len(header[i].split('_')) == 3 else header[i]): latestFilledData[i] for i in range(len(header))}
+            data = {(CFC_areaCoords[header[i].split('_')[1]] if header[i].split('_')[1] in [key for key in
+                                                                                            CFC_areaCoords] and len(
+                header[i].split('_')) == 3 else ""): float(latestFilledData[i])*radMult for i in range(len(header))}
+            del data[""]
+            print(data)
+            return(data)
 
-    else:
-        print(f"Failed to fetch CSV data. Status code: {response.status_code}")
-#
+        else:
+            print(f"Failed to fetch CSV data. Status code: {response.status_code}")
+    elif (query == "solarFlux"):
+        return(get_flux_data())
+    elif (query == "ozone"):
+        return(get_ozone_data())
+
 # @app.route('/', methods=['GET'])
 # def serve_index():
 #     return send_from_directory('pages', 'index.tsx',  mimetype='text/plain' )
